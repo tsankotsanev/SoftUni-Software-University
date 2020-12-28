8bd1af3d01103c5d0d7dcc6b1be7f17d10108a19
whitez9 <tsanko.tsanev.business@gmail.com>
2020-12-28 20:03:56 +0200

    Finish the task

--- CSharp-Programming-Fundamentals/Homework/Lists/HouseParty/Program.cs
+++ CSharp-Programming-Fundamentals/Homework/Lists/HouseParty/Program.cs
@@ -1,10 +1,53 @@
-﻿namespace HouseParty
+﻿using System;
+using System.Collections.Generic;
+
+namespace HouseParty
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var commandsCount = int.Parse(Console.ReadLine());
 
+            var guests = new List<string>();
+
+            for (var i = 0; i < commandsCount; i++)
+            {
+                var message = Console.ReadLine()
+                    .Split(" ",
+                    StringSplitOptions.RemoveEmptyEntries);
+
+                var name = message[0];
+                var action = message[2];
+
+                if (action == "not")
+                {
+                    if (guests.Contains(name))
+                    {
+                        guests.Remove(name);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{name} is not in the list!");
+                    }
+                }
+                else
+                {
+                    if (guests.Contains(name))
+                    {
+                        Console.WriteLine($"{name} is already in the list!");
+                    }
+                    else
+                    {
+                        guests.Add(name);
+                    }
+                }
+            }
+
+            foreach (var guest in guests)
+            {
+                Console.WriteLine(guest);
+            }
         }
     }
 }
