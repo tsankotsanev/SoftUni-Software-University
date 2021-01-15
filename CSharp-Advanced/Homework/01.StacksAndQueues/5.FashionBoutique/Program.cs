138ae865d7d35e21fe554cf868ceabfd7cc6e00c
tsankotsanev <tsanko.tsanev.business@gmail.com>
2021-01-15 23:04:03 +0200

    Add next semi solved task

---
+++ CSharp-Advanced/Homework/01.StacksAndQueues/5.FashionBoutique/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5.FashionBoutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var clothes = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var orderedClothes = new Stack<int>(clothes);
+            var rackCapacity = int.Parse(Console.ReadLine());
+            var clothesValues = 0;
+            var racksCount = 1;
+
+            while (orderedClothes.Count != 0)
+            {
+                var currentClothing = orderedClothes.Peek();
+
+                if (clothesValues + currentClothing < rackCapacity)
+                {
+                    clothesValues += currentClothing;
+                    orderedClothes.Pop();
+                }
+                else
+                {
+                    racksCount++;
+                    clothesValues = 0;
+                }
+            }
+
+            Console.WriteLine(racksCount);
+        }
+    }
+}
