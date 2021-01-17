e69ef21d4fa636484ca99560eda86bc552d20699
tsankotsanev <tsanko.tsanev.business@gmail.com>
2021-01-17 07:09:59 +0200

    Add next solved homework tasks

--- CSharp-Advanced/Homework/01.StacksAndQueues/07.TruckTour/Program.cs
+++ CSharp-Advanced/Homework/01.StacksAndQueues/07.TruckTour/Program.cs
@@ -1,10 +1,54 @@
-﻿namespace _07.TruckTour
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.TruckTour
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var pumpsData = new Queue<string>();
+            var pumpsCount = int.Parse(Console.ReadLine());
 
+            for (var i = 0; i < pumpsCount; i++)
+            {
+                pumpsData.Enqueue(Console.ReadLine());
+            }
+
+            for (var i = 0; i < pumpsCount; i++)
+            {
+                var currentPetrolAmount = 0;
+                var isSuccessful = true;
+
+                for (var j = 0; j < pumpsCount; j++)
+                {
+                    var pumpDataString = pumpsData.Dequeue();
+                    var pumpData = pumpDataString
+                        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse)
+                        .ToArray();
+
+                    pumpsData.Enqueue(pumpDataString);
+
+                    currentPetrolAmount += pumpData[0];
+                    currentPetrolAmount -= pumpData[1];
+
+                    if (currentPetrolAmount < 0)
+                    {
+                        isSuccessful = false;
+                    }
+                }
+
+                if (isSuccessful)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+
+                var tempData = pumpsData.Dequeue();
+                pumpsData.Enqueue(tempData);
+            }
         }
     }
 }
---
+++ CSharp-Advanced/Homework/01.StacksAndQueues/08.BalancedParentheses/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08.BalancedParentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var openBrackets = new Stack<char>();
+            var brackets = Console.ReadLine();
+            var closingBracketsCount = 0;
+            var openBracketsCount = 0;
+            var isBalanced = true;
+
+            foreach (var bracket in brackets)
+            {
+                if (bracket == '(' || bracket == '[' || bracket == '{')
+                {
+                    openBrackets.Push(bracket);
+                    openBracketsCount++;
+                }
+                else
+                {
+                    closingBracketsCount++;
+
+                    if (closingBracketsCount <= openBracketsCount)
+                    {
+                        var roundBrackets = bracket == ')' && openBrackets.Pop() == '(';
+                        var boxBrackets = bracket == ']' && openBrackets.Pop() == '[';
+                        var curlyBrackets = bracket == '}' && openBrackets.Pop() == '{';
+
+                        if (!roundBrackets && !boxBrackets && !curlyBrackets)
+                        {
+                            isBalanced = false;
+                        }
+                    }
+                    else
+                    {
+                        isBalanced = false;
+                    }
+                }
+            }
+
+            Console.WriteLine(isBalanced ? "YES" : "NO");
+        }
+    }
+}
