667c430d04bd97e89a7b2f09da7f88560bfa7bbf
Tsanko Tsanev <whitez@students.softuni.bg>
2021-01-29 23:03:01 +0200

    Update task

--- CSharp-Advanced/Homework/02.MultidimensionalArrays/01.DiagonalDifference/Program.cs
+++ CSharp-Advanced/Homework/02.MultidimensionalArrays/01.DiagonalDifference/Program.cs
@@ -8,13 +8,13 @@ namespace _01.DiagonalDifference
         static void Main(string[] args)
         {
             var n = int.Parse(Console.ReadLine());
-            var matrix = new int [n, n];
+            var matrix = new int[n, n];
 
             for (var row = 0; row < matrix.GetLength(0); row++)
             {
                 var rowData = Console
                     .ReadLine()
-                    ?.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
                     .Select(int.Parse)
                     .ToArray();
 
@@ -26,13 +26,13 @@ namespace _01.DiagonalDifference
 
             var primaryDiagonal = 0;
             var secondaryDiagonal = 0;
-            var counter = n -1 ;
+            var counter = n - 1;
 
             for (var row = 0; row < n; row++)
             {
                 primaryDiagonal += matrix[row, row];
                 secondaryDiagonal += matrix[row, counter];
-                
+
                 counter--;
             }
 
