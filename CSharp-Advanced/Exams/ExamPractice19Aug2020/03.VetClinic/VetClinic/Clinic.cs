2e3482471522560fe1961f49054f1bec37b70f04
Tsanko Tsanev <whitez@students.softuni.bg>
2021-07-09 19:46:09 +0300

    Add exam preparation

---
+++ CSharp-Advanced/Exams/ExamPractice19Aug2020/03.VetClinic/VetClinic/Clinic.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VetClinic
+{
+    public class Clinic
+    {
+        private List<Pet> data;
+
+        public Clinic(int capacity)
+        {
+            Capacity = capacity;
+            data = new List<Pet>();
+        }
+
+        public int Capacity { get; set; }
+
+        public int Count => data.Count;
+
+        public void Add(Pet pet)
+        {
+            if (data.Count < Capacity)
+            {
+                data.Add(pet);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            var pet = data.FirstOrDefault(p => p.Name == name);
+            if (pet == null)
+            {
+                return false;
+            }
+
+            data.Remove(pet);
+            return true;
+        }
+
+        public Pet GetPet(string name, string owner)
+        {
+            var pet = data.FirstOrDefault(p => p.Name == name && p.Owner == owner);
+
+            return pet;
+        }
+
+        public Pet GetOldestPet()
+        {
+            return data.OrderByDescending(p => p.Age).FirstOrDefault();
+        }
+
+        public string GetStatistics()
+        {
+            var result = new StringBuilder();
+            result.AppendLine($"The clinic has the following patients:");
+
+            foreach (var pet in data)
+            {
+                result.AppendLine($"Pet {pet.Name} with owner: {pet.Owner}");
+            }
+
+            return result.ToString();
+        }
+    }
+}
