c4ce66e7e06ac6ae8e891a99d1cefa8d57da9c2e
Tsanko Tsanev <whitez@students.softuni.bg>
2021-08-04 07:46:46 +0300

    Add exam preparation

---
+++ CSharp-Advanced/Exams/ExamPractice20Feb2021/02.Warships/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace _02.Warships
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
